01afebfec3154f6ef97d545d0c8295cdabe72af3
Shibu <shibu@decentraland.org>
2021-09-17 11:49:30 -0300

    add mushrooms

--- src/game.ts
+++ src/game.ts
@@ -3,20 +3,89 @@
 import { isPreviewMode } from "@decentraland/EnvironmentAPI"
 import { FestivalManagement } from "./concert/festivalMGMT"
 
-/*
-//add fewo
 
-export let fewo = new Entity()
-fewo.addComponent(new GLTFShape('models/fewo.glb'))
-fewo.addComponent(
+//add mushrooms
+
+export let mushrooms = new Entity()
+mushrooms.addComponent(new GLTFShape('models/mushrooms.glb'))
+mushrooms.addComponent(
   new Transform({
-    position: new Vector3(48, 3, 64),
+    position: new Vector3(48, 0, 64),
     rotation: Quaternion.Euler(0, 90, 0),
-    scale: new Vector3(10,10,10)
+    scale: new Vector3(1,1,1)
   })
 )
-engine.addEntity(fewo) */
+engine.addEntity(mushrooms) 
 
+
+//add mushrooms_02
+
+export let mushrooms_02 = new Entity()
+mushrooms_02.addComponent(new GLTFShape('models/mushrooms_02.glb'))
+mushrooms_02.addComponent(
+  new Transform({
+    position: new Vector3(48, 0, 64),
+    rotation: Quaternion.Euler(0, 90, 0),
+    scale: new Vector3(1,1,1)
+  })
+)
+engine.addEntity(mushrooms_02) 
+
+
+//add mushrooms_03
+
+export let mushrooms_03 = new Entity()
+mushrooms_03.addComponent(new GLTFShape('models/mushrooms_03.glb'))
+mushrooms_03.addComponent(
+  new Transform({
+    position: new Vector3(48, 0, 64),
+    rotation: Quaternion.Euler(0, 90, 0),
+    scale: new Vector3(1,1,1)
+  })
+)
+engine.addEntity(mushrooms_03) 
+
+
+//add mushrooms_04
+
+export let mushrooms_04 = new Entity()
+mushrooms_04.addComponent(new GLTFShape('models/mushrooms.glb'))
+mushrooms_04.addComponent(
+  new Transform({
+    position: new Vector3(20, 15, 64),
+    rotation: Quaternion.Euler(0, 45, 0),
+    scale: new Vector3(1,1,1)
+  })
+)
+engine.addEntity(mushrooms_04) 
+
+
+//add mushrooms_05
+
+export let mushrooms_05 = new Entity()
+mushrooms_05.addComponent(new GLTFShape('models/mushrooms_02.glb'))
+mushrooms_05.addComponent(
+  new Transform({
+    position: new Vector3(80, 30, 60),
+    rotation: Quaternion.Euler(0, 145, 0),
+    scale: new Vector3(1,1,1)
+  })
+)
+engine.addEntity(mushrooms_05) 
+
+
+//add mushroom_06
+
+export let mushroom_06 = new Entity()
+mushroom_06.addComponent(new GLTFShape('models/mushrooms_03.glb'))
+mushroom_06.addComponent(
+  new Transform({
+    position: new Vector3(30, 8, 60),
+    rotation: Quaternion.Euler(0, 120, 0),
+    scale: new Vector3(1,1,1)
+  })
+)
+engine.addEntity(mushroom_06) 
 
 executeTask(async ()=>{
   if(await isPreviewMode()){
--- src/resources.ts
+++ src/resources.ts
@@ -25,17 +25,18 @@ export default {
         screens:[ 
         {position: new Vector3(15,22.8,64), rotation: Quaternion.Euler(0,90,0), scale: new Vector3(23.1,23.1,1)},
 
-        {position: new Vector3(26.3,26.5,21.9), rotation: Quaternion.Euler(15,60,0), scale: new Vector3(11.9,11.8,-7)},
-        {position: new Vector3(29.7,10.4,28.5), rotation: Quaternion.Euler(0,60,0), scale: new Vector3(15.1,15.4,1)},
+        {position: new Vector3(26.3,26.5+6.5,21.9), rotation: Quaternion.Euler(15,60,0), scale: new Vector3(11.9,11.8,-7)},
+        {position: new Vector3(29.7,10.4+1.2,28.5), rotation: Quaternion.Euler(0,60,0), scale: new Vector3(15.1,15.4,1)},
         {position: new Vector3(44.4,21.5,19.2), rotation: Quaternion.Euler(22.912,42.537,358.212), scale: new Vector3(7.5,7.5,1)},
-        {position: new Vector3(27.8,21.8,34.9), rotation: Quaternion.Euler(12.998,58.974,359.769), scale: new Vector3(6.8,6.7,-7)},
+        {position: new Vector3(27.8,21.8+2.5,34.9), rotation: Quaternion.Euler(12.998,58.974,359.769), scale: new Vector3(6.8,6.7,-7)},
 
-        {position: new Vector3(26.3,26.5,106.1), rotation: Quaternion.Euler(15,120,360), scale: new Vector3(11.9,11.8,-7)},
-        {position: new Vector3(29.7,10.4,99.5), rotation: Quaternion.Euler(0,120,0), scale: new Vector3(15.1,15.4,1)},
+        {position: new Vector3(26.3,26.5+6.5,106.1), rotation: Quaternion.Euler(15,120,360), scale: new Vector3(11.9,11.8,-7)},
+        {position: new Vector3(29.7,10.4+1.2,99.5), rotation: Quaternion.Euler(0,120,0), scale: new Vector3(15.1,15.4,1)},
         {position: new Vector3(44.4,21.5,108.8), rotation: Quaternion.Euler(22.912,137.463,358.212), scale: new Vector3(7.5,7.5,1)},
-        {position: new Vector3(27.8,21.8,93.1), rotation: Quaternion.Euler(12.998,121.026,359.769), scale: new Vector3(6.8,6.7,-7)}
+        {position: new Vector3(27.8,21.8+2.5,93.1), rotation: Quaternion.Euler(12.998,121.026,359.769), scale: new Vector3(6.8,6.7,-7)}
         ],
-
+    },
+/*
         danceAreas:[
             {transform: {position: new Vector3(50,1,24), rotation: Quaternion.Euler(90,0,0), scale: new Vector3(11,10,1)}, type: "robot"},
             {transform: {position: new Vector3(79,1,43), rotation: Quaternion.Euler(90,0,0), scale: new Vector3(11,10,1)}, type: "tik"},
@@ -49,10 +50,11 @@ export default {
         ]
     },
 
+*/
     animationStates:{
         lights: ["Lights_Tentacles_01","Lights_Tentacles_02","Lights_Tentacles_03","Lights_Tentacles_04"],
         lightsBack: ["Light_01_Action","Light_02_Action","Light_03_Action"],
         lightsColumns: ["ArmatureAction"],
         waterColumns: ["Armature.003Action"]
     }
-}
+    }
