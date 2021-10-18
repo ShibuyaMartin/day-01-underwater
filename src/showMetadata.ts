3dd6d250491c8a31c397a939247067e9e59a3900
nearnshaw <nearnshaw@decentraland.org>
2021-10-18 12:29:43 -0300

    3lau & rac subs

--- src/showMetadata.ts
+++ src/showMetadata.ts
@@ -1,5 +1,5 @@
-import { subtitleString } from './subtitle-files/BPMtoSubs1'
-import { subtitleString2 } from './subtitle-files/OliverDemoMerged2'
+import { subs3Lau } from './subtitle-files/merge3lau'
+import { racSubs } from './subtitle-files/mergeRac'
 import { MiyaSubs } from './subtitle-files/MergeBPM_MIYA_MIYA'
 import { NinaSubs } from './subtitle-files/mergedNina'
 
@@ -13,10 +13,10 @@ export type showType = {
 
 export const subtitlesList = [
   MiyaSubs,
-  subtitleString2,
-  subtitleString,
-  subtitleString,
-  subtitleString,
+  MiyaSubs,
+  MiyaSubs,
+  racSubs,
+  subs3Lau,
   NinaSubs,
 ]
 
@@ -25,14 +25,14 @@ export let FAKING_LOCALLY: boolean = true
 export const shows: any = {
   deadmau5: {
     link: `https://player.vimeo.com/external/616165133.m3u8?s=b904d959337c905551285b3fbd7325bc077638d7`,
-    subs: subtitleString,
+    subs: racSubs,
     startTime: 1633650252,
     length: 47,
   },
   nina: { link: 'videos/NinaNesbitt720.mp4' },
   test: {
     link: 'videos/OliverResumeBPMTest.mp4',
-    subs: subtitleString2,
+    subs: NinaSubs,
     startTime: 1633650252,
     length: 15,
   },
